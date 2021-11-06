51ea31ec16e7d0e1641a46eac9dde9ca000d6f52
John Walsh <88987709+wjohn564@users.noreply.github.com>
2021-11-06 21:22:59 +0000

    Uploaded Customer class
    Set variables with get and set methods

---
+++ Customer.java
@@ -0,0 +1,46 @@
+public class Customer {
+    private String firstName;
+    private String secondName;
+    private int phone;
+    private String email;
+
+    Customer(String firstName, String secondName, int phone, String email) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.phone = phone;
+        this.email = email;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
